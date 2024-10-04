0ce3543415531332abbc989c3f7b99ddcb655199
isaac242628 <isaac.souza26@etec.sp.gov.br>
2024-10-04 19:07:29 -0300

    colocado

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+# Construa uma função que receba uma data no formato DD/MM/AAAA e devolva uma string no formato D de mesPorExtenso de AAAA. 
+#Opcionalmente, valide a data e retorne NULL caso a data seja inválida.
 
+mes = ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Setembro', 'Outubro', 'Novembro', 'Dezembro']
+def separando_Valor(data):
+    
+    D, MM, AAA = map(int, data.split('/'))
+    return D, MM, AAA
+    
+
+data = input()
+print(separando_Valor(data))
