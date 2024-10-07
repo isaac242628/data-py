acb66ae977b21c515c2de65a1f463ba19ccc34cc
Isaac <isaac.souza26@etec.sp.gov.br>
2024-10-07 19:55:00 -0300

    atividade 2

---
+++ embaralhar_palavras.py
@@ -0,0 +1,11 @@
+# Construa uma função que receba uma string como parâmetro e devolva outra string com os carateres embaralhados. 
+# Por exemplo: se função receber a palavra python, pode retornar npthyo, ophtyn ou qualquer outra combinação possível, de forma aleatória. Padronize em sua função que todos os caracteres serão devolvidos em caixa alta ou caixa baixa, independentemente de como foram digitados.
+
+
+
+
+
+palavra = input()
+leia = len(palavra)
+print(leia)
+print(palavra.lower())
